77f36f5c4d529bf2daef603274ece72faee632b7
blackroa <thatsuwutdhi_jud@hotmail.com>
2015-04-28 23:48:45 +0700

    program1.c

--- program1.c
+++ program1.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-int kuy()
+int eiei()
 {
 int i;
 i = 50;
@@ -8,7 +8,7 @@ return i;
 void main()
 {
 int a;
-a = kuy();
+a = eiei();
 printf("hello githubA\n");
 printf("modify by a\n");
 }
